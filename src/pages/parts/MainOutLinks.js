858d5d25f55505294cd77497dedeb3d106ee2c3a
the0bob <bob@bobpalmer.info>
2021-05-05 21:43:22 -0500

    init

---
+++ src/pages/parts/MainOutLinks.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import OutLink from './OutLink';
+
+export default function MainOutLinks({color}) {
+  let style = { color };
+  return(
+    <ul className="list-inline">
+      <li className="list-inline-item">
+        <OutLink href="https://www.linkedin.com/in/im-bob/">
+          <i className="fab fa-linkedin fa-fw fa-3x" style={style}></i>
+        </OutLink>
+      </li>
+      <li className="list-inline-item">
+        <OutLink href="https://github.com/the0bob">
+          <i className="fab fa-github fa-fw fa-3x" style={style}></i>
+        </OutLink>
+      </li>
+      <li className="list-inline-item">
+        <OutLink href="https://codepen.io/the0bob/">
+          <i className="fab fa-codepen fa-fw fa-3x" style={style}></i>
+        </OutLink>
+      </li>
+      <li className="list-inline-item">
+        <OutLink href="https://www.hackerrank.com/the0bob/">
+          <i className="fab fa-hackerrank fa-fw fa-3x" style={style}></i>
+        </OutLink>
+      </li>
+    </ul>
+  );
+}
---
+++ src/pages/parts/index.js
@@ -0,0 +1,12 @@
+import Header from './Header';
+import About from './About';
+
+import OutLink from './OutLink';
+import MainOutLinks from './MainOutLinks';
+import CtaButton from './CtaButton';
+import Spacer from './Spacer';
+
+export { 
+  Header, About,
+  OutLink, MainOutLinks, CtaButton, Spacer 
+};
